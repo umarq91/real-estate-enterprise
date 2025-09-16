2672576834e680c2e096fbb7cbdb38d975d6c58b
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2025-09-16 16:38:00 +0500

    add navbar and hero section

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/navbar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
