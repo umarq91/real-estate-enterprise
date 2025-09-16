1e5b6841b769437547fa9a24ef301204f22b239d
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2025-09-16 16:46:47 +0500

    finish landing page sections

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import HeroSection from "./hero-section";
+import FeatureSection from "./feature-section";
+import DiscoverSection from "./discover-section";
+import CallToActionSection from "./cta";
+import FooterSection from "./footer";
 
 const LandingPage = () => {
   return (
     <div>
       <HeroSection />
+      <FeatureSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
     </div>
   );
 };
